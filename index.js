702cb03fc9270fad4d4f7605bc0aa00d06980d1c
lnardon <lucaslnardon@gmail.com>
2021-01-01 20:44:17 -0300

    ✨ Add base card info structure

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // IMPORTS
 import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.118/build/three.module.js";
-import { OrbitControls } from "https://cdn.jsdelivr.net/npm/three@0.118/examples/jsm/controls/OrbitControls.js";
 import { OBJLoader } from "https://cdn.jsdelivr.net/npm/three@0.118/examples/jsm/loaders/OBJLoader.js";
 import { MTLLoader } from "https://cdn.jsdelivr.net/npm/three@0.118/examples/jsm/loaders/MTLLoader.js";
 
@@ -26,34 +25,27 @@ const camera = new THREE.PerspectiveCamera(
 camera.position.z = 100;
 
 //LIGHTS
-const light1 = new THREE.AmbientLight(0xffffff, 0.8);
-const light2 = new THREE.PointLight(0xffffff, 1);
-
-scene.add(light1);
-scene.add(light2);
+const pointLight = new THREE.PointLight(0xffffff, 1);
+pointLight.position.set(0, 20, 0);
+scene.add(pointLight);
 
 //OBJECT
-const loader = new OBJLoader();
-const mtlLoader = new MTLLoader();
-let ps5;
-mtlLoader.load("./ps5.mtl", (materials) => {
-  materials.preload();
-  loader.setResourcePath("./textures/");
-  loader.setMaterials(materials);
-  loader.load("./ps5.obj", (object) => {
-    ps5 = object;
-    ps5.scale.set(10, 10, 10);
-    scene.add(ps5);
-  });
-});
+// const loader = new OBJLoader();
+// const mtlLoader = new MTLLoader();
+// let card;
+// mtlLoader.load("./card.mtl", (materials) => {
+//   materials.preload();
+//   loader.setMaterials(materials);
+//   loader.load("./card.obj", (object) => {
+//     card = object;
+//     scene.add(card);
+//   });
+// });
 
 //RENDER LOOP
 requestAnimationFrame(render);
 
 function render() {
-  if (ps5) {
-    ps5.rotateY(Math.PI / 300);
-  }
   renderer.render(scene, camera);
   requestAnimationFrame(render);
 }
@@ -67,3 +59,16 @@ window.addEventListener(
   },
   false
 );
+
+// Event Listeners
+document.getElementById("name").addEventListener("keypress", (e) => {
+  // Change Card Name Model
+});
+
+document.getElementById("number").addEventListener("keypress", (e) => {
+  // Change Card number Model
+});
+
+document.getElementById("expiration").addEventListener("keypress", (e) => {
+  // Change Card Expiration Model
+});
